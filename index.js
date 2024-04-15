c32630b494074a651a6102dc81eb091813a406ad
ammarafridi-max <ammar.afridi95@gmail.com>
2024-04-15 15:37:35 +0400

    Use env for email, and frontend url

--- index.js
+++ index.js
@@ -12,18 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://my-dummy-ticket.vercel.app",
+    origin: process.env.FRONTEND_URL,
   })
 );
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://my-dummy-ticket.vercel.app/"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
 
 // Nodemailer
 let transporter = nodemailer.createTransport({
@@ -59,7 +50,7 @@ app.post("/", (req, res) => {
 
       // Send email
       let mailOptions = {
-        from: "ammar.afridi95@gmail.com",
+        from: process.env.FRONTEND_URL,
         to: "info@citytours.ae",
         subject: `New Form Submitted On MyDummyTicket.ae`,
         text: `${
