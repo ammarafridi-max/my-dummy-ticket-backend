8e0b9c79e9e9a2303e1e4425a0d0a471bdf12f01
ammarafridi-max <ammar.afridi95@gmail.com>
2024-04-15 15:43:37 +0400

    Update vercel.json

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL.replace(/\/$/, ""),
   })
 );
 
