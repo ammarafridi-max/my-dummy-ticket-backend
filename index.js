811aa53b3b27b489e626ac4d5ab441cec2ef1e4c
ammarafridi-max <ammar.afridi95@gmail.com>
2024-04-15 15:26:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://my-dummy-ticket.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
