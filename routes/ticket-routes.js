b44f1abb39e0a13e924ddd09560f2afa9993eef6
ammarafridi-max <ammar.afridi95@gmail.com>
2024-05-10 00:20:13 +0400

    Backend logic changed

--- routes/ticket-routes.js
+++ routes/ticket-routes.js
@@ -31,69 +31,84 @@ router.post("/", async (req, res) => {
       message: req.body.message,
     };
 
-    // 5. Send email
-    let mailOptions = {
-      from: process.env.SENDER_EMAIL,
-
-      to: "info@citytours.ae",
-
-      subject: `${
-        formData.passengers[0].firstName + " " + formData.passengers[0].lastName
-      } Submitted a Form On MyDummyTicket.ae`,
-
-      html: `<p><strong>Type:</strong> ${
-        formData.ticketType
-      }<br> ${formData.passengers.map((passenger, index) => {
-        return `
-                <strong>Passenger ${index + 1}:</strong> ${passenger.title}
-                ${passenger.firstName} ${passenger.lastName}<br>
-              `;
-      })}<strong>Number of Tickets:</strong> ${
-        formData.quantity
-      }; <br><strong>Phone Number:</strong> ${
-        formData.phoneNumber
-      } <br><strong>Email:</strong> ${
-        formData.email
-      } <br><strong>From:</strong> ${formData.from} <br><strong>To:</strong> ${
-        formData.to
-      } <br><strong>Departing on:</strong> ${formData.departureDate}; ${
-        formData.arrivalDate &&
-        ` <br><strong>Departing on:</strong> ${formData.arrivalDate}`
-      } <br>${
-        formData.message && `<strong>Message:</strong> ${formData.message}`
-      } </p>`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error("Error occurred:", error.message);
-        return;
-      }
-      console.log("Email sent successfully!");
-    });
-
     // 2. Stripe Checkout session
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price: formData.ticketId,
-          quantity: formData.quantity,
-        },
-      ],
-      mode: "payment",
-      success_url: `${process.env.FRONTEND_URL}?success=true`,
-      cancel_url: `${process.env.FRONTEND_URL}`,
-    });
+    const session = await stripe.checkout.sessions.create(
+      {
+        line_items: [
+          {
+            price: formData.ticketId,
+            quantity: formData.quantity,
+          },
+        ],
+        mode: "payment",
+        success_url: `${process.env.FRONTEND_URL}?success=true`,
+        cancel_url: `${process.env.FRONTEND_URL}`,
+      },
+      async (err, session) => {
+        if (err) {
+          console.error("Error creating Stripe session:", err);
+          return res
+            .status(500)
+            .json({ error: "An unexpected error occurred" });
+        }
 
-    // 3. Send response to client
-    res.status(200).json({
-      sessionId: session.id,
-      clientSecret: session.client_secret,
-      url: session.url,
-    });
+        try {
+          // 3. Send response to client
+          res.status(200).json({
+            sessionId: session.id,
+            clientSecret: session.client_secret,
+            url: session.url,
+          });
 
-    // 4. Send data to database.
-    // await FormModel.create(formData);
+          // 4. Send data to database.
+          await FormModel.create(formData);
+
+          // 5. Send email
+          let mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: "info@citytours.ae",
+            subject: `${
+              formData.passengers[0].firstName +
+              " " +
+              formData.passengers[0].lastName
+            } Submitted a Form On MyDummyTicket.ae`,
+            html: `<p><strong>Type:</strong> ${
+              formData.ticketType
+            }<br> ${formData.passengers.map((passenger, index) => {
+              return ` <strong>Passenger ${index + 1}:</strong> ${
+                passenger.title
+              } ${passenger.firstName} ${passenger.lastName}<br> `;
+            })}<strong>Number of Tickets:</strong> ${
+              formData.quantity
+            }; <br><strong>Phone Number:</strong> ${
+              formData.phoneNumber
+            } <br><strong>Email:</strong> ${
+              formData.email
+            } <br><strong>From:</strong> ${
+              formData.from
+            } <br><strong>To:</strong> ${
+              formData.to
+            } <br><strong>Departing on:</strong> ${formData.departureDate}; ${
+              formData.arrivalDate &&
+              ` <br><strong>Departing on:</strong> ${formData.arrivalDate}`
+            } <br>${
+              formData.message &&
+              `<strong>Message:</strong> ${formData.message}`
+            } </p>`,
+          };
+
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.error("Error occurred:", error.message);
+              return;
+            }
+            console.log("Email sent successfully!");
+          });
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+    );
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "An unexpected error occurred" });
