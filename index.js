3a517fbc181b251d1d99f8294912d74528d14752
ammarafridi-max <ammar.afridi95@gmail.com>
2024-11-19 11:34:14 +0400

    Comment removed

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ app.use((req, res, next) => {
 
 connectDB();
 
-// Routes
-
 app.use("/api/ticket", ticketRoutes);
 app.use("/api/airports", airportRoutes);
 app.use("/api/flights", flightRoutes);
