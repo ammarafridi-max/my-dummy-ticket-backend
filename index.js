b45c094c33dd381a71f07e601d801667c5a04ae2
ammarafridi-max <ammar.afridi95@gmail.com>
2024-12-08 12:35:07 +0400

    X-session-id added to cors

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.all('*', function (req, res, next) {
     : cors.default;
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Session-ID, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
