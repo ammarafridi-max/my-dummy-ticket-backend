874b5791f29f1572699241428194166c8c9b16e5
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-08 14:59:04 +0400

    Add routes for publish and duplicate

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -9,6 +9,7 @@ const {
   updateBlogPost,
   deleteBlogPost,
   publishBlog,
+  duplicateBlogPost,
 } = require('../controllers/blog.controller');
 const { protect, restrictTo } = require('../middleware/auth.middleware');
 
@@ -17,9 +18,10 @@ router.get('/slug/:slug', getBlogPostBySlug);
 
 router.use(protect, restrictTo('admin', 'author', 'blogManager'));
 router.post('/', upload.single('coverImage'), createBlogPost);
-router.patch('/:id/publish', publishBlog);
 router.get('/:id', getBlogPostById);
 router.patch('/:id', upload.single('newCoverImage'), updateBlogPost);
 router.delete('/:id', deleteBlogPost);
+router.patch('/:id/publish', publishBlog);
+router.post('/:id/duplicate', duplicateBlogPost);
 
 module.exports = router;
