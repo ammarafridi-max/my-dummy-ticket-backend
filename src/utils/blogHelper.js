1ed3c1f268045eefcde50665020f823cbaee290d
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:58:15 +0400

    Remove manual slugify

--- src/utils/blogHelper.js
+++ src/utils/blogHelper.js
@@ -1,15 +1,5 @@
 const Blog = require('../models/Blog');
 
-exports.slugify = (text = '') => {
-  return text
-    .toString()
-    .toLowerCase()
-    .trim()
-    .replace(/[\s\_]+/g, '-')
-    .replace(/[^a-z0-9\-]/g, '')
-    .replace(/\-+/g, '-');
-};
-
 exports.estimateReadingTime = (content = '') => {
   const words = content.trim().split(/\s+/).length || 0;
   const wordsPerMinute = 200;
@@ -20,16 +10,12 @@ exports.generateUniqueSlug = async (baseSlug, currentId = null) => {
   let slug = baseSlug;
   let counter = 1;
 
-  let exists = await Blog.exists(
-    currentId ? { slug, _id: { $ne: currentId } } : { slug }
-  );
+  let exists = await Blog.exists(currentId ? { slug, _id: { $ne: currentId } } : { slug });
 
   while (exists) {
     slug = `${baseSlug}-${counter}`;
     counter += 1;
-    exists = await Blog.exists(
-      currentId ? { slug, _id: { $ne: currentId } } : { slug }
-    );
+    exists = await Blog.exists(currentId ? { slug, _id: { $ne: currentId } } : { slug });
   }
 
   return slug;
