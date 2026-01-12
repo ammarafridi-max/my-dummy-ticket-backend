935852ae1129a2285e8a57cce878ec8bb5eb5dea
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:56:41 +0400

    File readability improvement

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
+
 const {
   createBlogPost,
   getBlogPosts,
@@ -11,12 +12,14 @@ const {
   publishBlog,
   duplicateBlogPost,
 } = require('../controllers/blog.controller');
+
 const { protect, restrictTo } = require('../middleware/auth.middleware');
 
 router.get('/', getBlogPosts);
 router.get('/slug/:slug', getBlogPostBySlug);
 
-router.use(protect, restrictTo('admin', 'author', 'blogManager'));
+router.use(protect, restrictTo('admin', 'blog-manager'));
+
 router.post('/', upload.single('coverImage'), createBlogPost);
 router.get('/:id', getBlogPostById);
 router.patch('/:id', upload.single('newCoverImage'), updateBlogPost);
