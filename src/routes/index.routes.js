66e416d0e890b76799e19c3c2997d953e190fed6
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:55:08 +0400

    Add routes for insurance routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const ticketRoutes = require('./ticket.routes');
+const insuranceRoutes = require('./insurance.routes');
 const airportRoutes = require('./airport.routes');
 const flightRoutes = require('./flight.routes');
 const userRoutes = require('./user.routes');
@@ -9,6 +9,7 @@ const blogRoutes = require('./blog.routes');
 const authRoutes = require('./auth.routes');
 
 router.use('/ticket', ticketRoutes);
+router.use('/insurance', insuranceRoutes);
 router.use('/airports', airportRoutes);
 router.use('/flights', flightRoutes);
 router.use('/users', userRoutes);
