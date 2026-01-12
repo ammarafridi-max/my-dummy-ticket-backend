9fff5a583d2318da965c1b58577ea053b0eea5aa
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:58:43 +0400

    Add api and secret key check

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
+if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+  console.warn('⚠️ Cloudinary credentials are missing');
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
