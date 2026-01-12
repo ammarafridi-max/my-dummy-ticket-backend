63fe3838ed4c121607a8d2037eb5e0e0d06d5db9
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:57:07 +0400

    File readability improvement

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -1,12 +1,9 @@
+const router = require('express').Router();
 const multer = require('multer');
-const express = require('express');
-const router = express.Router();
 const emailController = require('../controllers/email.controller');
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router
-  .route('/send-email')
-  .post(upload.single('reservation'), emailController.sendEmail);
+router.route('/send-email').post(upload.single('reservation'), emailController.sendEmail);
 
 module.exports = router;
