432700ab273312c39fd7f2bd4d274a93e217f80c
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:52:21 +0400

    Response json object update

--- src/controllers/airport.controller.js
+++ src/controllers/airport.controller.js
@@ -3,21 +3,19 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.fetchAirports = catchAsync(async (req, res, next) => {
-  const keyword = req.query.keyword;
+  const { keyword } = req.query;
 
-  if (!keyword) {
+  if (!keyword || keyword.length < 3) {
     return next(new AppError('Airport keyword is required and must be at least 3 characters long.', 400));
   }
 
   const response = await amadeus.referenceData.locations.get({
     subType: 'AIRPORT',
-    keyword: keyword,
+    keyword,
   });
 
-  const data = response.data;
-
-  return res.status(200).json({
-    message: 'airports list fetched successfully',
-    result: data,
+  res.status(200).json({
+    status: 'success',
+    data: response.data,
   });
 });
