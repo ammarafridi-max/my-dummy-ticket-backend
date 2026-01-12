1c44e18dc92a8cb4238a1a9101c27ac74bcfc419
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:55:31 +0400

    Change route names

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -1,23 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+const router = require('express').Router();
 const ticketController = require('../controllers/ticket.controller');
 const { protect, restrictTo } = require('../middleware/auth.middleware');
-const router = express.Router();
 
-router
-  .route('/')
-  .get(protect, restrictTo('admin', 'agent'), ticketController.getAllTickets)
-  .post(ticketController.createTicketRequest);
+router.post('/', ticketController.createTicketRequest);
+router.post('/checkout', ticketController.createStripePaymentUrl);
+router.get('/:sessionId', ticketController.getTicket);
 
-router.post('/buy-ticket', ticketController.createStripePaymentUrl);
+router.use(protect, restrictTo('admin', 'agent'));
 
-router
-  .route('/:sessionId')
-  .get(ticketController.getTicket)
-  .delete(protect, restrictTo('admin', 'agent'), ticketController.deleteTicket);
-
-router
-  .route('/:sessionId/updateOrderStatus')
-  .patch(protect, restrictTo('admin', 'agent'), ticketController.updateOrderStatus);
+router.get('/', ticketController.getAllTickets);
+router.delete('/:sessionId', ticketController.deleteTicket);
+router.patch('/:sessionId/status', ticketController.updateOrderStatus);
 
 module.exports = router;
