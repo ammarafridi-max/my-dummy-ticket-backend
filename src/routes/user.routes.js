c7ca04e69b5bcd928be94f9336a5f691b06c0752
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-12 18:55:40 +0400

    Change route names

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,12 +4,15 @@ const authController = require('../controllers/auth.controller');
 const { protect, restrictTo } = require('../middleware/auth.middleware');
 
 router.use(protect);
-router.get('/myAccount', authController.currentUserInfo);
-router.patch('/updateMyAccount', authController.updateCurrentUser);
-router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', authController.currentUserInfo);
+router.patch('/me', authController.updateCurrentUser);
+router.patch('/me/password', authController.updatePassword);
 
 router.use(restrictTo('admin'));
+
 router.route('/').get(userController.getUsers).post(userController.createUser);
+
 router
   .route('/:username')
   .get(userController.getUser)
