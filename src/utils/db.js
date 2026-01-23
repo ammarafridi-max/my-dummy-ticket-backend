554ba937f03b2760faa99922e1618009a94c32f4
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-23 17:01:19 +0400

    Create file for DB connection

---
+++ src/utils/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDB(context = 'app') {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`🗄️ MongoDB connected (${context})`);
+  } catch (err) {
+    console.error(`❌ MongoDB connection failed (${context})`, err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
