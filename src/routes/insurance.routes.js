c8985f55d07369fbe1029a318256aa4ac66f8190
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-23 17:10:23 +0400

    Create routes for finalize, nationalities, and policy downloading

--- src/routes/insurance.routes.js
+++ src/routes/insurance.routes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { getInsuranceQuotes } = require('../controllers/insurance.controller');
+const {
+  getInsuranceQuotes,
+  finalizeInsurance,
+  createNationalities,
+  getNationalities,
+  getInsuranceApplication,
+  downloadInsurancePolicy,
+} = require('../controllers/insurance.controller');
 
 router.route('/quote').post(getInsuranceQuotes);
+router.route('/finalize').post(finalizeInsurance);
+router.route('/nationalities').post(createNationalities);
+router.route('/nationalities').get(getNationalities);
+router.route('/download/:policyId').get(downloadInsurancePolicy);
+router.route('/:sessionId').get(getInsuranceApplication);
 
 module.exports = router;
