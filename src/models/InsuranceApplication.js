02d67a0fa81d3fa39f36ea45d7c2a331aeaf87eb
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-23 17:08:11 +0400

    Create Insurance Application model

---
+++ src/models/InsuranceApplication.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+const PassengerSchema = new mongoose.Schema(
+  {
+    type: String,
+    title: String,
+    firstName: String,
+    lastName: String,
+    dob: String,
+    nationality: String,
+    passport: String,
+  },
+  { _id: false },
+);
+
+const InsuranceApplicationSchema = new mongoose.Schema(
+  {
+    sessionId: { type: String, default: uuidv4, unique: true },
+    journeyType: {
+      type: String,
+      required: true,
+      enum: ['single', 'annual', 'biennial'],
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    region: {
+      id: { type: String, enum: ['gulf', 'europe', 'subcon', 'worldwide_ex', 'worldwide']},
+      name: { type: String },
+      description: { type: String },
+    },
+    quantity: {
+      adults: { type: Number },
+      children: { type: Number },
+      infants: { type: Number },
+    },
+    passengers: [PassengerSchema],
+    email: { type: String, required: true, match: /^\S+@\S+\.\S+$/ },
+    mobile: {
+      code: { type: String },
+      digits: { type: String },
+    },
+    schemeId: { type: Number },
+    quoteId: { type: Number },
+    policyId: { type: String },
+    policyNumber: { type: String },
+    paymentStatus: { type: String, enum: ['PAID', 'UNPAID'], default: 'UNPAID' },
+    amountPaid: {
+      currency: { type: String },
+      amount: { type: Number },
+    },
+    reviewEmailSent: { type: Boolean, default: false }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+module.exports = mongoose.model('insurance-application', InsuranceApplicationSchema);
