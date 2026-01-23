81e3588056fcd635609dfdc5fbbf59a13d3a2a9a
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-23 17:06:50 +0400

    Move operations to user service

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,10 @@
-const User = require('../models/User');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const userService = require('../services/user.service');
 
-const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach((key) => {
-    if (allowedFields.includes(key)) newObj[key] = obj[key];
-  });
-  return newObj;
-};
+exports.getUsers = catchAsync(async (req, res) => {
+  const users = await userService.getUsers();
 
-exports.getUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find({});
   res.status(200).json({
     status: 'success',
     message: 'Users fetched successfully',
@@ -20,9 +13,8 @@ exports.getUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getUser = catchAsync(async (req, res, next) => {
-  const { username } = req.params;
-  const user = await User.findOne({ username });
-  if (!user) return next(new AppError('Could not find user!', 404));
+  const user = await userService.getUserByUsername(req.params.username);
+
   res.status(200).json({
     status: 'success',
     message: 'User found successfully',
@@ -30,29 +22,17 @@ exports.getUser = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createUser = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+exports.createUser = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+
   res.status(201).json({
     status: 'success',
     data: user,
   });
 });
 
-exports.updateUser = catchAsync(async (req, res, next) => {
-  const { username } = req.params;
-  if (!username)
-    return next(new AppError("Please provide a user's username", 401));
-  if (req.body.password)
-    return next(
-      new AppError('Please use /updateMyPassword to change password', 400)
-    );
-
-  const user = await User.findOneAndUpdate({ username }, req.body, {
-    runValidators: true,
-    new: true,
-  });
-
-  if (!user) return next(new AppError('User not found.', 404));
+exports.updateUser = catchAsync(async (req, res) => {
+  const user = await userService.updateUserByUsername(req.params.username, req.body);
 
   res.status(200).json({
     status: 'success',
@@ -61,20 +41,11 @@ exports.updateUser = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteUser = catchAsync(async (req, res, next) => {
-  const { username } = req.params;
-  if (!username)
-    return res.json({ status: 'fail', message: 'Username is missing.' });
-
-  const user = await User.findOneAndDelete({ username });
-  if (!user) {
-    return res
-      .status(404)
-      .json({ status: 'fail', message: 'User not found with that id.' });
-  }
+exports.deleteUser = catchAsync(async (req, res) => {
+  const user = await userService.deleteUserByUsername(req.params.username);
 
   res.status(200).json({
     status: 'success',
-    message: `User ${username} deleted successfully`,
+    message: `User ${user.username} deleted successfully`,
   });
 });
