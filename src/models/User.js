54f78fcc8750d98ee2e8fea0e6d68e4d907b69b5
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-23 17:08:51 +0400

    Validation improvement

--- src/models/User.js
+++ src/models/User.js
@@ -2,48 +2,63 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide a name for the user'],
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a name for the user'],
+      trim: true,
+      maxlength: 100,
+    },
+
+    username: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, 'Username is required'],
+      trim: true,
+      minlength: [8, 'Username must be at least 8 characters long'],
+      maxlength: 50,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, 'Email is required'],
+      trim: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long'],
+      select: false,
+    },
+
+    role: {
+      type: String,
+      lowercase: true,
+      enum: ['admin', 'agent', 'blog-manager'],
+      default: 'agent',
+      required: true,
+    },
+
+    status: {
+      type: String,
+      uppercase: true,
+      enum: ['ACTIVE', 'INACTIVE'],
+      default: 'ACTIVE',
+      index: true,
+    },
   },
-  username: {
-    type: String,
-    lowercase: true,
-    unique: true,
-    minlength: [8, 'Username must be at least 8 characters long'],
-    required: [true, 'Username is required'],
-    trim: true,
+  {
+    timestamps: true,
   },
-  email: {
-    type: String,
-    lowercase: true,
-    unique: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
-    required: [true, 'Email is required'],
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters long'],
-    select: false,
-  },
-  role: {
-    type: String,
-    lowercase: true,
-    enum: ['admin', 'agent', 'blog-manager'],
-    default: 'agent',
-    required: true,
-  },
-  status: {
-    type: String,
-    uppercase: true,
-    enum: ['ACTIVE', 'INACTIVE'],
-    default: 'ACTIVE',
-  },
-});
+);
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
@@ -51,10 +66,7 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-userSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
-) {
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
   return bcrypt.compare(candidatePassword, userPassword);
 };
 
